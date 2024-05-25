ba622eff4a848500c4c709095e231fe96ac35773
Facundo <facundo.sydransky@ing.austral.edu.ar>
2024-05-25 12:09:08 -0300

    product-testCorrections

--- prisma/migrations/20240524021129_order_to_product_correction/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/product/product.service.spec.ts
@@ -0,0 +1,45 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { ProductService } from './product.service';
+import { ProductRepositoryMock } from './product.repositoryMock';
+describe('orderService.spec.ts', () => {
+    let app: INestApplication;
+    let productService: ProductService;
+    let productRepositoryMock: ProductRepositoryMock;
+
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+            providers: [
+                ProductService,
+                ProductRepositoryMock
+            ],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+        productService = moduleFixture.get<ProductService>(ProductService);
+        productRepositoryMock = moduleFixture.get<ProductRepositoryMock>(ProductRepositoryMock);
+
+    });
+
+
+    it('001_creatingAproduct', async () => {
+        const newProd = await productService.createProduct({
+            productId: '1',
+            price: 100,
+            qty: 1,
+            suppliers: []
+        });
+
+        expect(newProd).toEqual({
+            id: '1',
+            price: 100,
+            qty: 1,
+            suppliers: []
+        });
+    });
+
+});
