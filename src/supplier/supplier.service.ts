6c76964abc80e6eaba51765d11f1c7fc1c8c102e
Facundo <facundo.sydransky@ing.austral.edu.ar>
2024-05-25 22:32:29 -0300

    mock-changes

--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -5,10 +5,13 @@ import { CreateSupplierDto } from './dto/create-supplier.dto';
 @Injectable()
 export class SupplierService {
   constructor(private readonly supplierRepository: SupplierRepository) {}
+
+  //manejar el error cuando el proveedor no existe
   findSupplier(id: string) {
     return this.supplierRepository.getSupplierById(id.toString());
   }
 
+  //falta manejar el error cuando el producto no tiene proveedores y cuando el producto no existe
   findAllbyProduct(id: string) {
     return this.supplierRepository.getSuppliersByProductId(id);
   }
