beef2c004f5a4d266086b6b06ee1fd3240a61b3c
quinhos <marco.castagnaro@ing.austral.edu.ar>
2024-05-26 14:50:37 -0300

    corrections and seeds

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -4,7 +4,7 @@ import { ProductService } from './product.service';
 import { ProductRepositoryMock } from './product.repositoryMock';
 import { ProductRepository } from './product.repository';
 import { PrismaService } from '../prisma/prisma.service';
-describe('orderService.spec.ts', () => {
+describe('productService.spec.ts', () => {
   let app: INestApplication;
   let productService: ProductService;
 
@@ -13,10 +13,10 @@ describe('orderService.spec.ts', () => {
       providers: [
         PrismaService,
         ProductService,
-          {
-              provide: ProductRepository,
-              useClass: ProductRepositoryMock,
-          },
+        {
+          provide: ProductRepository,
+          useClass: ProductRepositoryMock,
+        },
       ],
     }).compile();
 
@@ -25,9 +25,9 @@ describe('orderService.spec.ts', () => {
     productService = moduleFixture.get<ProductService>(ProductService);
   });
 
-    afterAll(async () => {
-        await app.close();
-    });
+  afterAll(async () => {
+    await app.close();
+  });
 
   it('001_creatingAproduct', async () => {
     const newProd = await productService.createProduct({
@@ -40,34 +40,34 @@ describe('orderService.spec.ts', () => {
   });
 
   it('002_creatingAproductWithMoreThan1qty', async () => {
-      const newProd = await productService.createProduct({
-          price: 100,
-          qty: 5,
-      });
+    const newProd = await productService.createProduct({
+      price: 100,
+      qty: 5,
+    });
 
-      expect(newProd.price).toEqual(100);
-      expect(newProd.qty).toEqual(5);
-  })
+    expect(newProd.price).toEqual(100);
+    expect(newProd.qty).toEqual(5);
+  });
 
-    it('003_checkIfThereIsStock', async () => {
-        const newProd = await productService.createProduct({
-            price: 100,
-            qty: 10,
-        });
+  it('003_checkIfThereIsStock', async () => {
+    const newProd = await productService.createProduct({
+      price: 100,
+      qty: 10,
+    });
 
-        const hasStock = await productService.checkIfThereIsStock(newProd.id, 5);
-        expect(hasStock).toEqual(true);
-    })
+    const hasStock = await productService.checkIfThereIsStock(newProd.id, 5);
+    expect(hasStock).toEqual(true);
+  });
 
-    it('004_checkIfThereIsStockWithMoreThanAvailable', async () => {
-        const newProd = await productService.createProduct({
-            price: 100,
-            qty: 10,
-        });
+  it('004_checkIfThereIsStockWithMoreThanAvailable', async () => {
+    const newProd = await productService.createProduct({
+      price: 100,
+      qty: 10,
+    });
 
-        const hasStock = await productService.checkIfThereIsStock(newProd.id, 15);
-        expect(hasStock).toEqual(false);
-    })
+    const hasStock = await productService.checkIfThereIsStock(newProd.id, 15);
+    expect(hasStock).toEqual(false);
+  });
 
   it('005_substractingStock', async () => {
     const newProd = await productService.createProduct({
@@ -79,21 +79,14 @@ describe('orderService.spec.ts', () => {
     expect(updatedProd.qty).toEqual(5);
   });
 
-
-    it('006_substractingStockWithMoreThanAvailable', async () => {
-        const newProd = await productService.createProduct({
-            price: 100,
-            qty: 10,
-        });
-
-        await expect(productService.substractStock(newProd.id, 15)).rejects.toThrow(`Insufficient stock for product ${newProd.id}. Requested: ${15}`);
-
+  it('006_substractingStockWithMoreThanAvailable', async () => {
+    const newProd = await productService.createProduct({
+      price: 100,
+      qty: 10,
     });
 
-
-
-
-
-
-
+    await expect(productService.substractStock(newProd.id, 15)).rejects.toThrow(
+      `Insufficient stock for product ${newProd.id}. Requested: ${15}`,
+    );
+  });
 });
