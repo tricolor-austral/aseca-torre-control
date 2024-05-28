283db55d306c74c3f2aecb20eb68cb65f99c8b13
Facundo <facundo.sydransky@ing.austral.edu.ar>
2024-05-27 23:59:37 -0300

    crossdocking-integration-final

--- prisma/seeds/buyerSeed.ts
+++ prisma/seeds/buyerSeed.ts
@@ -1,32 +1,56 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-async function main() {
-  // Seed para Buyers
-  for (let i = 1; i <= 10; i++) {
-    await prisma.buyer.create({
-      data: {
-        id: `buyer_${i.toString()}`, // Convertir el índice a cadena
-        name: 'Nombre del Comprador',
-      },
-    });
-  }
+  async function main() {
+    // Seed para Buyers
+ /*   for (let i = 1; i <= 10; i++) {
+      await prisma.buyer.create({
+        data: {
+          id: `bugher_${i.toString()}`, // Convertir el índice a cadena
+          name: 'Nombre del Comprador',
+        },
+      });
+    }
+*/
+    // Seed para Productos
+    const productos = [
+      {id: "11", price: 10.99, qty: 100},
+      {id: "p1", price: 20.99, qty: 10000},
+      {id: "p2", price: 5.99, qty: 100000},
+      {id: "4", price: 15.99, qty: 75},
+      {id: "5", price: 8.99, qty: 150},
+    ];
 
-  // Seed para Productos
-  const productos = [
-    { price: 10.99, qty: 100 },
-    { price: 20.99, qty: 50 },
-    { price: 5.99, qty: 200 },
-    { price: 15.99, qty: 75 },
-    { price: 8.99, qty: 150 },
-  ];
 
-  for (const producto of productos) {
-    await prisma.product.create({
-      data: producto,
-    });
+    for (const producto of productos) {
+      await prisma.product.create({
+        data: producto,
+      });
+    }
+
+    //seed para suppliers
+    for (let i = 1; i <= 10; i++) {
+      await prisma.supplier.create({
+        data: {
+          id: `subbblier_${i.toString()}`, // Convertir el índice a cadena
+          name: 'suplier name',
+          products: {
+            connect: [{
+              id: `p1`
+              // Convertir el índice a cadena
+            },
+              {
+                id: `p2`
+              }
+            ]
+          }
+
+        },
+      });
+    }
+
+
   }
-}
 
 main()
   .catch((e) => {
--- src/cross-docking/cross-docking.service.ts
+++ src/cross-docking/cross-docking.service.ts
@@ -4,6 +4,7 @@ import {SupplierService} from '../supplier/supplier.service';
 import {crossDockingOrder} from "./dto/crossDockingOrder";
 import {ProductService} from "../product/product.service";
 import {CreateOrderDto, CreateSuborderDto, ProductAmountCreate} from "./dto/crossDockingDto";
+import axios from "axios";
 
 @Injectable()
 export class CrossDockingService {
@@ -11,34 +12,43 @@ export class CrossDockingService {
 
   async sendOrderToCrossDocking(orderDto: crossDockingOrder) {
       const json = await this.sendJson(orderDto);
-      const path = "https://5437-2800-af0-1409-3117-c11d-3c1f-dca-2a9e.ngrok-free.app/order/create";
-      console.log(JSON.stringify(json))
-      await fetch(path, {method: 'POST', body: JSON.stringify(json), headers: {'Content-Type': 'application/json'}})
+      const path = "https://03d7-2800-af0-1409-3117-c11d-3c1f-dca-2a9e.ngrok-free.app/order/create";
+      try {
+          await axios.post(path, json);
+
+
+      }
+      // Handle errors
+      catch (error) {
+          console.error('Fetch failed:', error);
+    }
   }
 
   private async sendJson(orderDto: crossDockingOrder) {
       //manejar error de supplier not found
-      const suppliers =   orderDto.productsId.map( async (productsId) => {
-          return await this.supplierService.findAllbyProduct(productsId)
-      });
-      const subOrders = this.generateSubOrders(suppliers, orderDto.productsId);
+      const suppliers =  new Array(orderDto.productsId.length)
+      for(let i = 0; i < orderDto.productsId.length; i++) {
+            suppliers[i] = await this.supplierService.findAllbyProduct(orderDto.productsId[i]);
+      }
+      const subOrders = await this.generateSubOrders(suppliers, orderDto.productsId);
       return new CreateOrderDto(orderDto.buyerId, subOrders);
 
 
   }
 
 
-  private generateSubOrders(suppliers, productsId) {
-      const products =  productsId.map(async (productId) => {
-          return await this.productService.getProductById(productId)
-      });
+  private async generateSubOrders(suppliers, productsId) {
+      const products = new Array(productsId.length);
+        for (let i = 0; i < productsId.length; i++) {
+            products[i] = await this.productService.getProductById(productsId[i]);
+        }
 
       const subOrders = new Array(products.length);
-            for (let i = 0; i < products.length; i++) {
-                const productAmountCreate = new ProductAmountCreate(products[i]);
-                subOrders.push(new CreateSuborderDto(suppliers[i], [productAmountCreate]));
-            }
-        return subOrders;
+      for (let i = 0; i < products.length; i++) {
+          const productAmountCreate = await new ProductAmountCreate(products[i]);
+          subOrders[i] = (new CreateSuborderDto(suppliers[i], [productAmountCreate]));
+      }
+      return subOrders;
   }
 
 
